eff49581a8cce9cfaf96c2be340686b883832196
OtaviOuu <conta.otaviou@gmail.com>
2025-03-15 00:54:22 -0300

    Implement main script for browser automation and scraping using GenimedesMonitor

---
+++ main.py
@@ -0,0 +1,26 @@
+import nodriver as uc
+from nodriver import *
+import asyncio
+from dotenv import load_dotenv
+from GenimedesMonitor import GenimedesMonitor
+
+
+async def load_browser(browser_name: str) -> uc.Browser:
+    browser = await uc.start(
+        headless=False,
+        browser_executable_path=f"/snap/bin/{browser_name}",
+        user_data_dir="./uc-user-data",
+    )
+    return browser
+
+
+async def main() -> None:
+    load_dotenv(override=True)
+
+    browser = await load_browser("chromium")
+    monitor = GenimedesMonitor(browser)
+    await monitor.scrape(browser)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())

98873e3edb4989e41b5403065b8cba6df4204b33
OtaviOuu <conta.otaviou@gmail.com>
2025-03-15 00:54:17 -0300

    Add GenimedesMonitor class for automated login and scraping

---
+++ GenimedesMonitor.py
@@ -0,0 +1,33 @@
+import nodriver as uc
+from nodriver import *
+import os
+
+
+class GenimedesMonitor:
+    def __init__(self, browser: uc.Browser):
+        self.main_url = "https://www2.ime.usp.br/ganimedes/public/login.xhtml"
+        self.browser = browser
+        self.user = os.getenv("USER")
+        self.password = os.getenv("PASS")
+
+    async def _login(self, page: uc.Tab) -> None:
+        user_input = await page.find("#loginUsuario")
+        await user_input.send_keys(self.user)
+
+        password_input = await page.find("#senhaUsuario")
+        await password_input.send_keys(self.password)
+
+        login_btn = await page.find("#botaoLogin")
+        await login_btn.mouse_click()
+        await page.wait(t=2)
+
+    async def _get_login_form(self, page: uc.Tab) -> None:
+        get_login_form_btn = await page.find(text="Entrar com Senha única USP")
+        await page.wait()
+        await get_login_form_btn.mouse_click()
+
+    async def scrape(self, browser: uc.Browser) -> None:
+        page = await browser.get(self.main_url)
+
+        await self._get_login_form(page)
+        await self._login(page)
